82ee4d1f28d5dae98eeccb6ebc92f4588dc78da9
gonzalo_fuentes <fubagg@kyndryl.com>
2024-12-02 09:52:25 -0300

    feat: add unit selector

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-function appController() {
+async function appController() {
+    let units = "celsius";
+
+    const unitSelector = document.getElementById('unit-selector');
+    unitSelector.addEventListener('change', (event) => {
+        const option = event.target.value;
+        units = option;
+        console.log(units);
+    })
+
+    function getTemp(temp) {
+        if (units === "celsius") {
+            return `${temp}°C`;
+        }
+        const fahrenheit = (temp * 9 / 5) + 32;
+        return `${fahrenheit}°F`;
+    }
     function buildUrl(location) {
         const url_base = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
         const url_options = '?unitGroup=metric&contentType=json';
@@ -12,9 +28,27 @@ function appController() {
     async function obtainWeather(location) {
         const url = buildUrl(location);
         const response = await fetch(url);
-        const weatherdata = await response.json();
-        return weatherdata;
+        const weatherData = await response.json();
+        return weatherData;
     }
-    console.log(obtainWeather('tokyo'));
+
+    function displayData(data) {
+        const main = document.getElementById('main');
+
+        const location = document.createElement('div');
+        location.classList.add('location');
+        location.innerText = `Weather forecast for: ${data.resolvedAddress}`;
+        main.appendChild(location);
+
+        const currentConditions = data.currentConditions;
+
+        const temp = document.createElement('div');
+        temp.classList.add('temp');
+        temp.innerText = `${getTemp(currentConditions.temp)}` 
+                        + ` / Feels like ${getTemp(currentConditions.feelslike)}`;
+        main.appendChild(temp);
+        console.log(data);
+    }
+    displayData(await obtainWeather('tokyo'));
 }
 appController();

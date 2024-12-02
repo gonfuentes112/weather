93864db55520415cdadb104754a47de8373f3225
gonzalo_fuentes <fubagg@kyndryl.com>
2024-12-02 08:21:30 -0300

    feat: add obtain weather function

---
+++ script.js
@@ -0,0 +1,17 @@
+function buildUrl(location) {
+    const url_base = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
+    const url_options = '?unitGroup=metric&contentType=json';
+    const api_key = 'UWK72QS3WHCYRRTJ7CQYPQCPD';
+    return url_base
+            + location
+            + url_options
+            + `&key=${api_key}`
+}
+
+async function obtainWeather(location) {
+    const url = buildUrl(location);
+    const response = await fetch(url);
+    const weatherdata = await response.json();
+    return weatherdata;
+}
+console.log(obtainWeather('tokyo'));
